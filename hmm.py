6726ce5bb4bc37ac5b44ccf1c9759092961e98af
Eli Sennesh <e.sennesh@northeastern.edu>
2018-06-21 14:14:12 -0400

    hmm: rewrite with HMM generative model
    Testing Done: sampled from HMM at the command line
    
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- hmm.py
+++ hmm.py
@@ -1,49 +1,34 @@
-import probtorch.model as pm
-import probtorch.distributions as pd
+#!/usr/bin/env python3
+
+import probtorch
+import torch
 
 import combinators
+import utils
 
-# class EnsembleHMM(pd.Model):
-#     """A HMM with shared parameters for multiple time series"""
+def init_hmm(num_states, T=1, trace=probtorch.Trace()):
+    num_particles = trace.num_particles if hasattr(trace, 'num_particles')\
+                    else 1
 
-#     def __init__(self, num_states):
+    pi0 = trace.dirichlet(torch.ones(num_states), name='\\Pi_0')
+    pi = torch.zeros(num_particles, num_states, num_states)
+    for k in range(num_states):
+        pi[:, k] = trace.dirichlet(torch.ones(num_states),
+                                   name='\\Pi_%d' % (k+1))
+    mu = trace.normal(torch.zeros(num_states), torch.ones(num_states),
+                      name='\\mu')
+    sigma = trace.log_normal(torch.zeros(num_states), torch.ones(num_states),
+                             name='\\sigma')
+    zs = torch.ones(num_particles, T+1, dtype=torch.long) * -1
+    zs[:, 0] = trace.variable(torch.distributions.Categorical, pi0, name='Z_0')
+    return pi, mu, sigma, zs
 
-#     def forward(self):
-#         hmm = self.init_hmm(dataset)
-#         return pm.Map(hmm)
+def hmm_step(zs, pi, mu, sigma, t, trace={}, conditions=utils.EMPTY_TRACE):
+    zs[:, t] = trace.variable(torch.distributions.Categorical,
+                              utils.particle_index(pi, zs[:, t-1]),
+                              name='Z_%d' % t)
+    trace.normal(utils.particle_index(mu, zs[:, t]),
+                 utils.particle_index(sigma, zs[:, t]),
+                 name='X_%d' % t, value=conditions['X_%d' % t])
+    return zs, pi, mu, sigma, t, trace
 
-class HmmInit(combinators.Model):
-    pass
-
-class HmmTrans(combinators.Model):
-    pass
-
-class HmmLikelihood(combinators.Conditionable):
-    pass
-
-
-class HmmGlobals(combinators.Model):
-    def _init__(self, num_states):
-        ...
-
-    def forward(self, data, states=None):
-        ...
-        return data, likelihood, state_init, state_trans
-
-
-class HmmStates(combinators.Model):
-    def __init__(self, num_states):
-        ...
-
-    def forward(self, data, likelihood, state_init, state_trans):
-        ...
-        return states
-
-
-hmm_globals = HmmGlobals(num_states, name='globals')
-hmm_states = HmmStates(num_states, name='states')
-hmm = pm.Chain(hmm_globals, hmm_locals, name='hmm') # ToDo: what is syntax for ensuring data is input to both globals and locals
-ensemble_hmm = pm.Map(hmm, name='ensemble')
-
-for batch in dataset:
-    (likelihood, state_init, state_trans), states = infer(ensemble_hmm, batch)
