e1c2b088b16dab81f6d945a726535bea13094071
Eli Sennesh <e.sennesh@northeastern.edu>
2018-06-27 17:17:43 -0400

    hmm: use params for initializing HMM
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- hmm.py
+++ hmm.py
@@ -6,19 +6,16 @@ import torch
 import combinators
 import utils
 
-def init_hmm(num_states, T=1, trace=probtorch.Trace()):
+def init_hmm(num_states, T=1, trace=probtorch.Trace(), params={}):
     num_particles = trace.num_particles if hasattr(trace, 'num_particles')\
                     else 1
 
-    pi0 = trace.dirichlet(torch.ones(num_states), name='\\Pi_0')
+    pi0 = trace.param_dirichlet(params, name='\\Pi_0')
     pi = torch.zeros(num_particles, num_states, num_states)
     for k in range(num_states):
-        pi[:, k] = trace.dirichlet(torch.ones(num_states),
-                                   name='\\Pi_%d' % (k+1))
-    mu = trace.normal(torch.zeros(num_states), torch.ones(num_states),
-                      name='\\mu')
-    sigma = trace.log_normal(torch.zeros(num_states), torch.ones(num_states),
-                             name='\\sigma')
+        pi[:, k] = trace.param_dirichlet(params, name='\\Pi_%d' % (k+1))
+    mu = trace.param_normal(params, name='\\mu')
+    sigma = torch.sqrt(trace.param_normal(params, name='\\sigma')**2)
     zs = torch.ones(num_particles, T+1, dtype=torch.long) * -1
     zs[:, 0] = trace.variable(torch.distributions.Categorical, pi0, name='Z_0')
     return pi, mu, sigma, zs
