353478845598e134423d7b2b8aed3b9e716fe051
Eli Sennesh <e.sennesh@northeastern.edu>
2018-06-27 17:20:50 -0400

    utils: save closed-over key variable correctly
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- utils.py
+++ utils.py
@@ -43,7 +43,8 @@ def _parameterize_trace_methods(transforms=PARAM_TRANSFORMS):
         if _inspect.isfunction(v):
             args = _inspect.signature(v).parameters.keys()
             if 'name' in args and 'value' in args:
-                def param_sample(self, params, name=None, value=None, **kwargs):
+                def param_sample(self, params, name=None, value=None, k=k,
+                                 **kwargs):
                     params = {**params[name].copy(), **kwargs}
                     for arg, val in params.items():
                         if arg in transforms:
