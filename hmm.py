9575d22f99b86e322503494c2056a6408b94d744
Eli Sennesh <e.sennesh@northeastern.edu>
2018-06-27 22:43:54 -0400

    hmm: de-Latexize Greek letter variable names
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- hmm.py
+++ hmm.py
@@ -11,12 +11,12 @@ def init_hmm(num_states, T=1, trace=probtorch.Trace(), params={}):
     num_particles = trace.num_particles if hasattr(trace, 'num_particles')\
                     else 1
 
-    pi0 = trace.param_dirichlet(params, name='\\Pi_0')
+    pi0 = trace.param_dirichlet(params, name='Pi_0')
     pi = torch.zeros(num_particles, num_states, num_states)
     for k in range(num_states):
-        pi[:, k] = trace.param_dirichlet(params, name='\\Pi_%d' % (k+1))
-    mu = trace.param_normal(params, name='\\mu')
-    sigma = torch.sqrt(trace.param_normal(params, name='\\sigma')**2)
+        pi[:, k] = trace.param_dirichlet(params, name='Pi_%d' % (k+1))
+    mu = trace.param_normal(params, name='mu')
+    sigma = torch.sqrt(trace.param_normal(params, name='sigma')**2)
     zs = torch.ones(num_particles, T+1, dtype=torch.long) * -1
     zs[:, 0] = trace.variable(torch.distributions.Categorical, pi0, name='Z_0')
     return zs, pi, mu, sigma
@@ -38,5 +38,5 @@ def hmm_retrace(zs, pi, mu, sigma, trace={}, conditions=utils.EMPTY_TRACE):
     for step in range(t):
         zs[:, step] = trace['Z_%d' % step].value
     for k in range(pi.shape[1]):
-        pi[:, k] = trace['\\Pi_%d' % k].value
+        pi[:, k] = trace['Pi_%d' % k].value
     return zs, pi, mu, sigma, trace
