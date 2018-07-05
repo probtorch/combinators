85b34cb3540f0bbcc232291d5fff9aac4aa47ae2
Eli Sennesh <e.sennesh@northeastern.edu>
2018-07-05 18:09:06 -0400

    combinators: consolidate trace passing and inference needs into Model
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- combinators.py
+++ combinators.py
@@ -28,20 +28,23 @@ class GraphingTrace(probtorch.stochastic.Trace):
         self._stack.pop()
 
 class Model(nn.Module):
-    def __init__(self, f, params_namespace=None, phi={}, theta={}):
+    def __init__(self, f, phi={}, theta={}):
         super(Model, self).__init__()
         self._function = f
-        self._params_namespace = params_namespace
+        self._trace = probtorch.Trace()
+        self._observations = utils.EMPTY_TRACE.copy()
+        self._parent = collections.defaultdict(lambda: None)
+        self.condition()
         self.register_args(phi, True)
         self.register_args(theta, False)
 
     @classmethod
     def _bind(cls, outer, inner):
         def result(*args, **kwargs):
-            trace = kwargs['trace']
+            this = kwargs['this']
             temp = inner(*args, **kwargs)
-            return outer(*temp, trace=trace) if isinstance(temp, tuple) else\
-                   outer(temp, trace=trace)
+            return outer(*temp, this=this) if isinstance(temp, tuple) else\
+                   outer(temp, this=this)
         return result
 
     @classmethod
@@ -61,83 +64,73 @@ class Model(nn.Module):
 
     @classmethod
     def partial(cls, func, *arguments, **keywords):
-        def result(*args, **kwargs):
+        def wrapper(*args, **kwargs):
             kwargs = {**kwargs, **keywords}
             return func(*arguments, *args, **kwargs)
-        return cls(result)
+        result = cls(wrapper)
+        if isinstance(func, cls):
+            result.add_module(func._function.__name__, func)
+        for arg in arguments:
+            if isinstance(arg, cls):
+                result.add_module(arg._function.__name__, arg)
+        for k, v in keywords.items():
+            if isinstance(v, cls):
+                result.add_module(k, v)
+        return result
+
+    def add_module(self, name, module):
+        super(Model, self).add_module(name, module)
+        if isinstance(module, Model):
+            module._parent['parent'] = self
+
+    @property
+    def parent(self):
+        return self._parent['parent']
+
+    @property
+    def ancestor(self):
+        result = self.parent
+        while result.parent is not None:
+            result = result.parent
+        return result
+
+    def _condition(self, trace, observations):
+        if trace is not None:
+            self._trace = trace
+        if observations is not None:
+            self._observations = observations
+
+    def condition(self, trace=None, observations=None):
+        self.apply(lambda m: m._condition(trace, observations))
+
+    @property
+    def trace(self):
+        return self._trace
+
+    @property
+    def observations(self):
+        return self._observations
 
     def register_args(self, args, trainable=True):
         for k, v in utils.vardict(args).items():
-            if self._params_namespace is not None:
-                k = self._params_namespace + '__' + k
             if trainable:
                 self.register_parameter(k, nn.Parameter(v))
             else:
                 self.register_buffer(k, v)
 
-    def args_vardict(self, keep_vars=False):
-        result = self.state_dict(keep_vars=keep_vars)
-        if self._params_namespace is not None:
-            prefix = self._params_namespace + '__'
-            result = {k[len(prefix):]: v for (k, v) in result.items()
-                      if k.startswith(prefix)}
-        return utils.vardict(result)
-
-    def kwargs_dict(self):
-        members = dict(self.__dict__, **self.state_dict(keep_vars=True))
-        return {k: v for k, v in members.items()
-                if k in inspect.signature(self._function).parameters.keys()}
-
-    def forward(self, *args, trace={}, **kwargs):
-        kwparams = {**self.kwargs_dict(), 'trace': trace, **kwargs}
-        if self._params_namespace is not None:
-            kwparams[self._params_namespace] = self.args_vardict(keep_vars=True)
-        if isinstance(trace, GraphingTrace):
-            trace.push(self)
-        result = self._function(*args, **kwparams)
-        if isinstance(trace, GraphingTrace):
-            trace.pop()
-        return result
-
-class Conditionable(Model):
-    def __init__(self, f, params_namespace=None):
-        super(Conditionable, self).__init__(f, params_namespace)
-
-    @classmethod
-    def _bind(cls, outer, inner):
-        def result(*args, **kwargs):
-            trace = kwargs['trace']
-            conditions = kwargs['conditions']
-            temp = inner(*args, **kwargs)
-            return outer(*temp, trace=trace, conditions=conditions)\
-                   if isinstance(temp, tuple)\
-                   else outer(temp, trace=trace, conditions=conditions)
-        return result
+    def args_vardict(self, keep_vars=True):
+        return utils.vardict(self.state_dict(keep_vars=keep_vars))
 
     def forward(self, *args, **kwargs):
-        return super(Conditionable, self).forward(*args, **kwargs)
-
-class Inference(Conditionable):
-    @classmethod
-    def _bind(cls, outer, inner):
-        def result(*args, **kwargs):
-            temp = inner(*args, **kwargs)
-            outer_kwargs = {'trace': temp[-1],
-                            'conditions': kwargs['conditions']}
-            return outer(*temp[:-1], **outer_kwargs)
+        kwargs = {**kwargs, 'this': self}
+        if isinstance(self.trace, GraphingTrace):
+            self.trace.push(self)
+        result = self._function(*args, **kwargs)
+        if isinstance(self.trace, GraphingTrace):
+            self.trace.pop()
         return result
 
-    def forward(self, *args, **kwargs):
-        trace = kwargs['trace']
-        result = super(Inference, self).forward(*args, **kwargs)
-        return result if isinstance(result, tuple) else (result, trace)
-
-def sequence(step, T, *args, trace={}, conditions={}):
+def sequence(step, T, *args, **kwargs):
     for t in range(T):
-        results = step(*args, t, trace=trace, conditions=conditions)
-        if isinstance(results, tuple):
-            args = results[:-1]
-            trace = results[-1]
-        else:
-            args = results
-    return results
+        args = step(*args, t, **kwargs)
+    return args
