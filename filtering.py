4bf6837e8101140315a03ccae4892766e6223459
Eli Sennesh <e.sennesh@northeastern.edu>
2018-07-13 15:45:31 -0400

    filtering.ForwardBackwardMessenger: add full forward-backward implementation
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- filtering.py
+++ filtering.py
@@ -62,3 +62,39 @@ class ForwardMessenger(combinators.Model):
                 self.observation_dists[current].log_prob(observed) +\
                 marginals[:, current]
         latent_note['forward_joint_marginals'] = alpha
+
+class ForwardBackwardMessenger(ForwardMessenger):
+    def _backward_step(self, t, T):
+        support = self.trace[self._latent % t].dist.enumerate_support()
+        support = range(len(support))
+        num_particles = self.trace[self._latent % t].value.shape[0]
+
+        if t == T:
+            final_note = self.trace.annotation(self.name, self._latent % t)
+            final_note['backward_joint_marginals'] = torch.zeros(num_particles,
+                                                                 len(support))
+        else:
+            next_note = self.trace.annotation(self.name, self._latent % (t+1))
+            current_note = self.trace.annotation(self.name, self._latent % t)
+            observed = self.trace[self._observation % t].value
+            beta = torch.zeros(num_particles, len(support), len(support))
+            for current, succ in itertools.product(support, support):
+                beta[:, current, succ] =\
+                    self.observation_dists[current].log_prob(observed) +\
+                    self.transition(current, succ) +\
+                    next_note['backward_joint_marginals'][:, succ]
+            current_note['backward_joint_marginals'] = log_sum_exp(beta, dim=-1)
+
+    def backward_pass(self, T):
+        for t in reversed(range(T)):
+            self._backward_step(t+1, T)
+
+    def _posterior_step(self, t):
+        note = self.trace.annotation(self.name, self._latent % (t+1))
+        return note['forward_joint_marginals']+note['backward_joint_marginals']
+
+    def smoothed_posterior(self, t, T):
+        posteriors = torch.stack([self._posterior_step(n) for n in range(T)],
+                                 dim=0)
+        denominator = log_sum_exp(posteriors, dim=0)
+        return posteriors[t] - denominator
