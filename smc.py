414b856202c4b39c811ddbf51fb0c68f90fbc53a
Eli Sennesh <e.sennesh@northeastern.edu>
2018-07-30 18:43:33 -0400

    smc.ParticleMH: turn ParticleMH into its own combinator class
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- smc.py
+++ smc.py
@@ -3,6 +3,7 @@
 import collections
 import logging
 
+import numpy as np
 import probtorch
 from probtorch.util import log_mean_exp
 import torch
@@ -67,34 +68,39 @@ def variational_smc(num_particles, model_init, smc_sequence, num_iterations,
 
     return inference, model_init.args_vardict()
 
-def particle_mh(num_particles, model_init, smc_sequence, num_iterations, data,
-                *args, use_cuda=True):
-    elbos = torch.zeros(num_iterations)
-    samples = list(range(num_iterations))
+class ParticleMH(combinators.Model):
+    def __init__(self, model_init, smc_sequence, num_iterations=1, trainable={},
+                 hyper={}):
+        smc_model = combinators.Model.compose(smc_sequence, model_init,
+                                              intermediate_name='initializer')
+        super(ParticleMH, self).__init__(smc_model, trainable, hyper)
+        self._num_iterations = num_iterations
+        self.add_module('_function', smc_model)
 
-    if torch.cuda.is_available() and use_cuda:
-        model_init.cuda()
-        smc_sequence.cuda()
+    def forward(self, *args, **kwargs):
+        elbos = torch.zeros(self._num_iterations)
+        samples = list(range(self._num_iterations))
+        smc_sequence = list(list(self._function.children())[1].children())[0]
 
-    for i in range(num_iterations):
-        inference = ResamplerTrace(num_particles)
+        for i in range(self._num_iterations):
+            num_particles = kwargs['trace'].num_particles if 'trace' in kwargs\
+                            else 1
+            kwargs['trace'] = ResamplerTrace(num_particles)
 
-        vs = model_init(*args, trace=inference, guide=data)
-        vs = smc_sequence(initializer=vs, trace=inference, guide=data)
+            vs = super(ParticleMH, self).forward(*args, **kwargs)
 
-        inference = smc_sequence.trace
-        elbo = list(smc_sequence.children())[0].marginal_log_likelihood()
+            elbo = smc_sequence.marginal_log_likelihood()
+            acceptance = torch.min(torch.ones(1), torch.exp(elbo - elbos[i-1]))
+            if torch.bernoulli(acceptance) == 1 or i == 0:
+                elbos[i] = elbo
+                samples[i] = vs
+            else:
+                elbos[i] = elbos[i-1]
+                samples[i] = samples[i-1]
 
-        acceptance = torch.min(torch.ones(1), torch.exp(elbo - elbos[i-1]))
-        if (torch.bernoulli(acceptance) == 1).sum() > 0 or i == 0:
-            elbos[i] = elbo
-            samples[i] = vs
-        else:
-            elbos[i] = elbos[i-1]
-            samples[i] = samples[i-1]
-
-    if torch.cuda.is_available() and use_cuda:
-        model_init.cpu()
-        smc_sequence.cpu()
-
-    return samples, elbos, inference
+        result = []
+        for i in range(self._num_iterations):
+            particle = np.random.randint(0, self.trace.num_particles)
+            result.append([v[particle] for v in samples[i]])
+        return [torch.stack([result[i][j] for i in range(self._num_iterations)],
+                            dim=0) for j, _ in enumerate(samples[0])], elbos
