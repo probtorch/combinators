984b01f87ce8841b98189fbc5d59277200c5894f
Eli Sennesh <e.sennesh@northeastern.edu>
2018-08-01 09:48:49 -0400

    bouncing_ball_data: make sure that transition probabilities are never nan
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- bouncing_ball_data.py
+++ bouncing_ball_data.py
@@ -44,7 +44,7 @@ def transition(A, old_hidden, new_hidden):
 def intialization(T, num_series, Boundary):
     x0 = Boundary * np.random.random(num_series) * np.random.choice([-1,1], size=num_series)
     y0 = Boundary * np.random.random(num_series) * np.random.choice([-1,1], size=num_series)
-    init_v = np.random.random((num_series, 2)) 
+    init_v = np.random.random((num_series, 2))
     v_norm = ((init_v **2 ).sum(1)) ** 0.5 ## compute norm for each initial velocity
     init_v = init_v / v_norm[:, None] ## to make the velocity lying on the unit circle
     init_v_rand_dir = init_v * np.random.choice([-1,1], size=(num_series,2))
@@ -77,5 +77,7 @@ def generate_data(T, dt, init_state, Boundary, signal_noise_ratio):
 #         time.sleep(0.2)
     Disp = (STATE[1:] - STATE[:T])[:, :2]
 
-    A = A / A.sum(1)[:, None]
+    Asum = A.sum(1)[:, None]
+    Asum = np.where(Asum > 0, Asum, np.ones(Asum.shape))
+    A = A / Asum
     return STATE, Disp, A
