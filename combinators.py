0d96a46c4419c338004687eb43f4488e5fc48bf7
Eli Sennesh <e.sennesh@northeastern.edu>
2018-09-17 23:23:05 -0400

    Revert "combinators.Model: assert no double-parenting"
    This reverts commit 6d26e5be4bdf469c575da7ca23a418a1c33977c3.

--- combinators.py
+++ combinators.py
@@ -279,7 +279,6 @@ class Model(nn.Module):
     def add_module(self, name, module):
         super(Model, self).add_module(name, module)
         if isinstance(module, Model):
-            assert not module._parent['parent']
             module._parent['parent'] = self
 
     @property
