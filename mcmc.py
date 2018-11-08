037891ba2560f7ec2fac0c2a19335924e269ed76
Eli Sennesh <e.sennesh@northeastern.edu>
2018-11-08 14:02:20 -0500

    mcmc.Collection: rewrite as an inference/sampling combinator
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- mcmc.py
+++ mcmc.py
@@ -5,54 +5,46 @@ import logging
 
 import numpy as np
 import probtorch
-from probtorch.util import log_mean_exp
 import torch
 
 import combinators
 
-class Collection(combinators.ModelSampler):
-    def __init__(self, sampler, num_samples, acceptance):
-        super(Collection, self).__init__()
-        self._sampler = sampler
+class Collection(combinators.InferenceSampler):
+    def __init__(self, sampler, num_samples):
+        super(Collection, self).__init__(sampler)
         self._num_samples = num_samples
-        self._acceptance = acceptance
-
-    @property
-    def name(self):
-        return 'Collection(%s, %d)' % (self.sampler.name, self.num_samples)
-
-    @property
-    def sampler(self):
-        return self._sampler
 
     @property
     def num_samples(self):
         return self._num_samples
 
-    @property
-    def acceptance(self):
-        return self._acceptance
+    def accept(self, results, trace):
+        raise NotImplementedError()
 
-    def _forward(self, *args, **kwargs):
+    def forward(self, *args, **kwargs):
         samples = []
         trace = kwargs.pop('trace')
         while len(samples) < self.num_samples:
             kwargs['trace'] = trace.extract(self.sampler.name)
-            sample, sample_trace = self._sampler(*args, **kwargs)
-            if self._acceptance(sample, sample_trace):
+            sample, sample_trace = self.sampler(*args, **kwargs)
+            if self.accept(sample, sample_trace):
                 trace.insert(str(len(samples)) + '/' + self.sampler.name,
                              sample_trace)
                 samples.append(sample)
         return samples, trace
 
-def independent_mh(model, num_samples):
-    weight = 0.0
-    def acceptance(_, trace):
-        nonlocal weight
+    def infer(self, results, trace):
+        return results, trace
+
+class IndependentMH(Collection):
+    def __init__(self, sampler, num_samples):
+        super(IndependentMH, self).__init__(sampler, num_samples)
+        self._weight = 0.0
+
+    def accept(self, results, trace):
         candidate = trace.marginal_log_likelihood()
-        alpha = torch.min(torch.zeros(1), candidate - weight)
-        result = weight == 0.0 or torch.bernoulli(torch.exp(alpha)) == 1
+        alpha = torch.min(torch.zeros(1), candidate - self._weight)
+        result = self._weight == 0.0 or torch.bernoulli(torch.exp(alpha)) == 1
         if result:
-            weight = candidate
+            self._weight = candidate
         return result
-    return Collection(model, num_samples, acceptance)
