32f469e433e6d0bffb72ecd2c9dc6e204c5ba495
Eli Sennesh <e.sennesh@northeastern.edu>
2018-11-18 13:53:36 -0500

    mcmc.MHMove: replace everything with the MHMove combinator from the paper
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- mcmc.py
+++ mcmc.py
@@ -9,49 +9,18 @@ import torch
 
 import combinators
 
-def stack_samples(samples):
-    for i in len(samples[0]):
-        yield torch.stack([sample[i] for sample in samples], dim=0)
-
-class Collection(combinators.InferenceSampler):
-    def __init__(self, sampler, num_samples):
-        super(Collection, self).__init__(sampler)
-        self._num_samples = num_samples
-
-    @property
-    def num_samples(self):
-        return self._num_samples
-
-    def accept(self, results, trace):
+class MHMove(combinators.InferenceSampler):
+    def propose(self, results, trace):
         raise NotImplementedError()
 
-    def sample_prehook(self, trace, *args, **kwargs):
-        return trace, args, kwargs
-
-    def forward(self, *args, **kwargs):
-        samples = []
-        trace = kwargs.pop('trace')
-        while len(samples) < self.num_samples:
-            kwargs['trace'] = trace.extract(self.sampler.name)
-            sample, sample_trace = self.sampler(*args, **kwargs)
-            if self.accept(sample, sample_trace):
-                trace.insert(str(len(samples)) + '/' + self.sampler.name,
-                             sample_trace)
-                samples.append(sample)
-        return samples, trace
-
     def sample_hook(self, results, trace):
-        return list(stack_samples(results)), trace
-
-class IndependentMH(Collection):
-    def __init__(self, sampler, num_samples):
-        super(IndependentMH, self).__init__(sampler, num_samples)
-        self._weight = 0.0
-
-    def accept(self, results, trace):
-        candidate = trace.marginal_log_likelihood()
-        alpha = torch.min(torch.zeros(1), candidate - self._weight)
-        result = self._weight == 0.0 or torch.bernoulli(torch.exp(alpha)) == 1
-        if result:
-            self._weight = candidate
-        return result
+        marginal = trace.marginal_log_likelihood()
+        candidate, candidate_trace, move_candidate, move_current =\
+            self.propose(results, trace)
+        candidate_marginal = candidate_trace.marginal_log_likelihood()
+        mh_ratio = (candidate_marginal - move_candidate) -\
+                   (marginal - move_current)
+        log_alpha = torch.min(torch.zeros(1), mh_ratio)
+        if torch.bernoulli(torch.exp(log_alpha)) == 1:
+            return candidate, candidate_trace
+        return results, trace
