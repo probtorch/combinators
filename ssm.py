40c85366bb1f1b0d8b5ca0d54b0146a15627bf6d
Eli Sennesh <e.sennesh@northeastern.edu>
2018-11-24 13:36:45 -0500

    ssm: rewrite to pass data as a keyword param
    Signed-off-by: Eli Sennesh <e.sennesh@northeastern.edu>

--- ssm.py
+++ ssm.py
@@ -4,20 +4,21 @@ import torch
 from torch.distributions import Normal
 from torch.nn.functional import softplus
 
-def init_ssm(trace=None, params=None):
+def init_ssm(trace=None, params=None, data={}):
     mu = trace.param_sample(Normal, params, name='mu')
     sigma = torch.sqrt(trace.param_sample(Normal, params, name='sigma')**2)
     delta = trace.param_sample(Normal, params, name='delta')
     z0 = trace.sample(Normal, mu, softplus(sigma), name='Z_0')
     return z0, mu, sigma, delta
 
-def ssm_step(theta, t, trace=None):
+def ssm_step(theta, t, trace=None, data={}):
     z_prev, mu, sigma, delta = theta
     t += 1
     z_current = trace.sample(Normal, z_prev + delta, softplus(sigma),
                              name='Z_%d' % t)
-    trace.sample(
+    trace.variable(
         Normal, z_current,
         torch.ones(*z_current.shape, device=z_current.device), name='X_%d' % t,
+        value=data.get('X_%d' % t)
     )
     return z_current, mu, sigma, delta
