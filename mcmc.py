efda187d6155f7f39e46abde4c648f76755f127f
Eli Sennesh <esennesh@ccis.neu.edu>
2018-12-29 21:05:26 -0500

    mcmc: specify a number of MH moves to perform
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- mcmc.py
+++ mcmc.py
@@ -12,10 +12,11 @@ import trace_tries
 import utils
 
 class MHMove(combinators.InferenceSampler):
-    def __init__(self, sampler):
+    def __init__(self, sampler, moves=1):
         super(MHMove, self).__init__(sampler)
         self._args = ()
         self._kwargs = {}
+        self._moves = moves
 
     def sample_prehook(self, trace, *args, **kwargs):
         self._args = args
@@ -23,25 +24,30 @@ class MHMove(combinators.InferenceSampler):
         return trace, args, kwargs
 
     def propose(self, results, trace):
-        self._args = ()
-        self._kwargs = {}
         raise NotImplementedError()
 
     def sample_hook(self, results, trace):
         marginal = trace.marginal_log_likelihood()
-        candidate, candidate_trace, move_candidate, move_current =\
-            self.propose(results, trace)
-        candidate_marginal = candidate_trace.marginal_log_likelihood()
-        mh_ratio = (candidate_marginal - move_candidate) -\
-                   (marginal - move_current)
-        log_alpha = torch.min(torch.zeros(1), mh_ratio)
-        if torch.bernoulli(torch.exp(log_alpha)) == 1:
-            return candidate, candidate_trace
+        for _ in range(self._moves):
+            candidate, candidate_trace, move_candidate, move_current =\
+                self.propose(results, trace)
+            candidate_marginal = candidate_trace.marginal_log_likelihood()
+            mh_ratio = (candidate_marginal - move_candidate) -\
+                       (marginal - move_current)
+            log_alpha = torch.min(torch.zeros(1), mh_ratio)
+            if torch.bernoulli(torch.exp(log_alpha)) == 1:
+                results = candidate
+                trace = candidate_trace
+                marginal = candidate_marginal
+        self._args = ()
+        self._kwargs = {}
         return results, trace
 
 class LightweightMH(MHMove):
     def propose(self, results, trace):
         rv_index = np.random.randint(len(trace))
+        while trace[rv_index].observed:
+            rv_index = np.random.randint(len(trace))
         candidate = trace[0:rv_index]
         move_current = utils.marginalize_all(trace[rv_index].log_prob)
         dist = trace[rv_index].dist
@@ -50,6 +56,4 @@ class LightweightMH(MHMove):
         results, candidate = self.sampler(*self._args, **self._kwargs,
                                           trace=candidate)
         move_candidate = utils.marginalize_all(rv.log_prob)
-        self._args = ()
-        self._kwargs = {}
         return results, candidate, move_candidate, move_current
