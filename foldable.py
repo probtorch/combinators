b89631926c4109144c99aa35fb2a56f5b884c812
Eli Sennesh <sennesh.e@husky.neu.edu>
2019-02-05 19:00:11 -0500

    foldable.Foldable: implement Foldable in the new style
    Signed-off-by: Eli Sennesh <sennesh.e@husky.neu.edu>

--- foldable.py
+++ foldable.py
@@ -7,32 +7,56 @@ import combinators
 import traces
 import utils
 
-class Foldable(combinators.ModelSampler):
-    def __init__(self, operator, initializer=None, **kwargs):
-        super(Foldable, self).__init__()
+class Foldable(combinators.Model):
+    def __init__(self, operator, initializer=None, iteration=0, **kwargs):
         assert isinstance(operator, combinators.Sampler)
+        super(Foldable, self).__init__(batch_shape=operator.batch_shape)
         self.add_module('operator', operator)
         if isinstance(initializer, combinators.Sampler):
             self.add_module('_initializer', initializer)
+            assert self.operator.batch_shape == self._initializer.batch_shape
         else:
             self._initializer = initializer
         self._kwargs = kwargs
+        self._iteration = iteration
 
     @property
     def name(self):
-        return 'Foldable(%s)' % self.operator.name
+        return 'Foldable(%s)' % str(self._iteration)
 
-    def _forward(self, *args, **kwargs):
+    def forward(self, *args, **kwargs):
+        trace = traces.Traces()
         if isinstance(self._initializer, combinators.Sampler):
-            seed, trace = self._initializer(**kwargs)
+            seed, init_trace, seed_weight = self._initializer(**kwargs)
+            trace.insert(self.name, init_trace)
         else:
             seed = self._initializer
-            trace = kwargs.pop('trace')
-        kwargs['trace'] = trace.extract(self.name + str(args))
-        result, op_trace = self.operator(seed, *args, **kwargs)
-        trace.insert(self.name + str(args), op_trace)
-        next_step = Foldable(self.operator, result, **self._kwargs)
-        return (result, next_step), trace
+            seed_weight = torch.zeros(self.batch_shape)
+        result, op_trace, weight = self.operator(seed, *args, **kwargs)
+        next_step = Foldable(self.operator, initializer=result,
+                             iteration=self._iteration + 1, **self._kwargs)
+
+        trace.insert(self.name, op_trace)
+        weight += seed_weight
+
+        return (result, next_step), trace, weight
+
+    def walk(self, f):
+        if isinstance(self._initializer, combinators.Sampler):
+            initializer = self._initializer.walk(f)
+        else:
+            initializer = self._initializer
+        return f(Foldable(self.operator.walk(f), initializer, **self._kwargs))
+
+    def cond(self, qs):
+        qs_operator = qs[self.name:]
+        if isinstance(self._initializer, combinators.Sampler):
+            qs_initializer = qs[self.name:]
+            initializer = self._initializer.cond(qs_initializer)
+        else:
+            initializer = self._initializer
+        return Foldable(self.operator.cond(qs_operator), initializer,
+                        **self._kwargs)
 
 class Reduce(combinators.ModelSampler):
     def __init__(self, folder, generator):
