a13dd4de90eb202f04886c2a4cf6cd9392026fdd
Eli Sennesh <esennesh@ccis.neu.edu>
2019-02-12 22:03:27 -0500

    combinators.Primitive: expand observations to match up with batch_shapes
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- combinators.py
+++ combinators.py
@@ -130,6 +130,10 @@ class Primitive(Model):
                 provenance = Provenance.REUSED
             else:
                 provenance = Provenance.OBSERVED
+                shared_shapes = utils.shared_sizes(value.shape,
+                                                   self.batch_shape)
+                if shared_shapes != self.batch_shape:
+                    value = value.expand(*self.batch_shape, *value.shape)
         else:
             provenance = Provenance.SAMPLED
         return self.p.variable(Dist, *args, **kwargs, name=name, value=value,
