9ecf6f37e9e03344f3b510e5cfe7b7f3073d4ae1
Eli Sennesh <sennesh.e@husky.neu.edu>
2019-02-20 13:46:09 -0500

    mcmc.MHMove: factor out kernel from abstract method to separate combinator form
    Signed-off-by: Eli Sennesh <sennesh.e@husky.neu.edu>

--- mcmc.py
+++ mcmc.py
@@ -13,12 +13,11 @@ import importance
 import utils
 
 class MHMove(combinators.Inference):
-    def __init__(self, sampler, moves=1):
+    def __init__(self, sampler, kernel, moves=1):
         super(MHMove, self).__init__(sampler)
+        assert isinstance(kernel, combinators.TransitionKernel)
+        self.add_module('kernel', kernel)
         self._moves = moves
-
-    def propose(self, results, graph, *args, **kwargs):
-        raise NotImplementedError()
 
     def forward(self, *args, **kwargs):
         zs, xi, w = self.sampler(*args, **kwargs)
@@ -28,7 +27,7 @@ class MHMove(combinators.Inference):
         marginal = utils.marginalize_all(w)
         for _ in range(self._moves):
             zsq, xiq, wq, move_proposed, move_current =\
-                self.propose(zs, xi, *args, **kwargs)
+                self.kernel(zs, xi, w, *args, **kwargs)
             marginal_q = utils.marginalize_all(wq)
             mh_ratio = (marginal_q - move_proposed) - (marginal - move_current)
             log_alpha = torch.min(torch.zeros(mh_ratio.shape), mh_ratio)
@@ -40,37 +39,11 @@ class MHMove(combinators.Inference):
             zs = zs[0]
         return zs, xi, w
 
-class LightweightMH(MHMove):
-    def propose(self, results, graph, *args, **kwargs):
-        t = np.random.randint(len(graph))
-        sampled = [k for k in graph[t] if not graph[t][k].observed]
-        while not sampled:
-            t = np.random.randint(len(graph))
-            sampled = [k for k in graph[t] if not graph[t][k].observed]
-
-        address = sampled[np.random.randint(len(sampled))]
-        candidate_trace = utils.slice_trace(graph[t], address)
-        candidate_graph = graph.graft(t, candidate_trace)
-
-        zs, xi, w = self.sampler.cond(candidate_graph)(*args, **kwargs)
-
-        predicate = lambda k, v: not v.observed
-        move_candidate = utils.marginalize_all(xi.log_joint())
-        move_candidate += torch.log(torch.tensor(float(
-            xi.num_variables(predicate=predicate)
-        )))
-        move_current = utils.marginalize_all(graph.log_joint())
-        move_current += torch.log(torch.tensor(float(
-            graph.num_variables(predicate=predicate)
-        )))
-
-        return zs, xi, w, move_candidate, move_current
-
     def walk(self, f):
-        return LightweightMH(self.sampler.walk(f), self._moves)
+        return MHMove(self.sampler.walk(f), self.kernel, self._moves)
 
     def cond(self, qs):
-        return LightweightMH(self.sampler.cond(qs), self._moves)
+        return MHMove(self.sampler.cond(qs), self.kernel, self._moves)
 
 def resample_move_smc(sampler, initializer=None, moves=1, mcmc=LightweightMH):
     fold = foldable.Foldable(sampler, initializer=initializer)
