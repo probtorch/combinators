0fcb4307d526c169a7b0f3d167f51398a61259f6
Eli Sennesh <sennesh.e@husky.neu.edu>
2019-02-20 14:51:03 -0500

    examples/gmm/gmm: relativize imports
    Signed-off-by: Eli Sennesh <sennesh.e@husky.neu.edu>

--- examples/gmm/gmm.py
+++ examples/gmm/gmm.py
@@ -3,17 +3,17 @@
 from torch.distributions import Categorical, Dirichlet, Normal
 from torch.nn.functional import softplus
 
-import combinators
-import utils
+import combinators.model as model
+import combinators.utils as utils
 
-class InitGmm(combinators.Primitive):
+class InitGmm(model.Primitive):
     def _forward(self, pi_name='Pi', **kwargs):
         pi = self.param_sample(Dirichlet, name=pi_name)
         mu = self.param_sample(Normal, name='mu')
         sigma = self.param_sample(Normal, name='sigma')
         return mu, sigma, pi
 
-class Gmm(combinators.Primitive):
+class Gmm(model.Primitive):
     def _forward(self, mu, sigma, pi, latent_name='Z', observable_name='X',
                  data={}):
         z = self.sample(Categorical, softplus(pi), name=latent_name)
