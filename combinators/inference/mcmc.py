21d53fce9fd1fa3b3cb0c6bbb476a84fcf86671a
Eli Sennesh <esennesh@ccis.neu.edu>
2019-02-20 21:48:32 -0500

    combinators.inference.mcmc.MHMove: rewrite according to conversation
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- combinators/inference/mcmc.py
+++ combinators/inference/mcmc.py
@@ -15,37 +15,37 @@ from ..sampler import Sampler
 from .. import utils
 
 class MHMove(Inference):
-    def __init__(self, sampler, kernel, moves=1):
-        super(MHMove, self).__init__(sampler)
+    def __init__(self, target, kernel, moves=1):
+        super(MHMove, self).__init__(target)
         assert isinstance(kernel, TransitionKernel)
         self.add_module('kernel', kernel)
         self._moves = moves
 
     def forward(self, *args, **kwargs):
-        zs, xi, w = self.sampler(*args, **kwargs)
+        zs, xi, log_weight = self.target(*args, **kwargs)
         multiple_zs = isinstance(zs, tuple)
         if not multiple_zs:
             zs = (zs,)
-        marginal = utils.marginalize_all(w)
+        marginal = utils.marginalize_all(log_weight)
         for _ in range(self._moves):
-            zsq, xiq, wq, move_proposed, move_current =\
-                self.kernel(zs, xi, w, *args, **kwargs)
-            marginal_q = utils.marginalize_all(wq)
+            zsq, xiq, log_weight_q, move_proposed, move_current =\
+                self.kernel(zs, xi, log_weight, *args, **kwargs)
+            marginal_q = utils.marginalize_all(log_weight_q)
             mh_ratio = (marginal_q - move_proposed) - (marginal - move_current)
             log_alpha = torch.min(torch.zeros(mh_ratio.shape), mh_ratio)
             if torch.bernoulli(torch.exp(log_alpha)) == 1:
                 zs = zsq
                 xi = xiq
-                w = wq
+                log_weight = log_weight_q
         if not multiple_zs:
             zs = zs[0]
-        return zs, xi, w
+        return zs, xi, log_weight
 
     def walk(self, f):
-        return MHMove(self.sampler.walk(f), self.kernel, self._moves)
+        return MHMove(self.target.walk(f), self.kernel, self._moves)
 
     def cond(self, qs):
-        return MHMove(self.sampler.cond(qs), self.kernel, self._moves)
+        return MHMove(self.target.cond(qs), self.kernel, self._moves)
 
 class LightweightKernel(TransitionKernel):
     def __init__(self, prior):
