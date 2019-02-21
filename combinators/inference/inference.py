f2c771d08784445995a7a8c060a08e3dda9492c6
Eli Sennesh <esennesh@ccis.neu.edu>
2019-02-20 22:04:53 -0500

    combinators.inference.inference.Importance: switch to log_weight from just w
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- combinators/inference/inference.py
+++ combinators/inference/inference.py
@@ -32,7 +32,7 @@ class Importance(Inference):
         self.add_module('proposal', proposal)
 
     def forward(self, *args, **kwargs):
-        _, xi, w = self.proposal(*args, **kwargs)
+        _, xi, log_weight = self.proposal(*args, **kwargs)
         return self.target.cond(xi)(*args, **kwargs)
 
     def walk(self, f):
