0a468311bb1e765474a2cd9767c4f8ec769eae8e
Eli Sennesh <sennesh.e@husky.neu.edu>
2019-02-25 16:26:31 -0500

    combinators.inference.mcmc: factor out LightweightKernel into a kernel module
    Signed-off-by: Eli Sennesh <sennesh.e@husky.neu.edu>

--- combinators/inference/mcmc.py
+++ combinators/inference/mcmc.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 
-import collections
-import logging
-
-import numpy as np
-import probtorch
 import torch
 
 from .inference import Inference
 from . import importance
 from ..kernel.kernel import TransitionKernel
+from ..kernel import mh
 from ..model import foldable
-from ..sampler import Sampler
 from .. import utils
 
 class MarkovChain(Inference):
@@ -59,36 +54,9 @@ class MHMove(MarkovChain):
 def mh_move(target, kernel, moves=1):
     return MHMove(target, kernel, moves=moves)
 
-class LightweightKernel(TransitionKernel):
-    def log_transition_prob(self, _, destination):
-        move = utils.marginalize_all(destination.log_joint())
-        move += torch.log(torch.tensor(float(
-            destination.num_variables(predicate=lambda k, v: not v.observed)
-        )))
-        return move
-
-    def forward(self, zs, xi, log_weight, *args, **kwargs):
-        sampled = []
-        while not sampled:
-            t = np.random.randint(len(xi))
-            sampled = [k for k in xi[t].variables() if not xi[t][k].observed]
-
-        key = sampled[np.random.randint(len(sampled))]
-        candidate_trace = utils.slice_trace(xi[t], key)
-        return xi.graft(t, candidate_trace)
-
-    def walk(self, f):
-        return f(LightweightKernel(self.batch_shape))
-
-    def cond(self, qs):
-        return LightweightKernel(self.batch_shape)
-
-    @property
-    def name(self):
-        return 'LightweightKernel'
-
 def lightweight_mh(target, moves=1):
-    return mh_move(target, LightweightKernel(target.batch_shape), moves=moves)
+    return mh_move(target, mh.LightweightKernel(target.batch_shape),
+                   moves=moves)
 
 def resample_move_smc(target, moves=1, mcmc=lightweight_mh):
     inference = lambda m: mcmc(importance.Resample(m), moves)
