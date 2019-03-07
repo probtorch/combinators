0d64141df9abc2bc3d534e528b7d044cca85a85a
Eli Sennesh <esennesh@ccis.neu.edu>
2019-03-06 22:17:07 -0500

    combinators.kernel.MHMoveKernel: add a class for a MH detailed-balance kernel
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- combinators/kernel/mh.py
+++ combinators/kernel/mh.py
@@ -3,6 +3,7 @@
 import numpy as np
 import torch
 
+from ..inference import importance
 from .kernel import TransitionKernel
 from .. import utils
 
@@ -39,3 +40,51 @@ class LightweightKernel(TransitionKernel):
 
 def lightweight_kernel(shape):
     return LightweightKernel(shape)
+
+class MHMoveKernel(TransitionKernel):
+    def __init__(self, target, kernel, moves=1, *args, **kwargs):
+        super(MHMoveKernel, self).__init__(
+            *args, batch_shape=target.batch_shape, **kwargs
+        )
+        assert isinstance(kernel, TransitionKernel)
+        self.add_module('kernel', kernel)
+        self.add_module('target', target)
+        self._moves = moves
+
+    def log_transition_prob(self, origin, destination, shape):
+        return self.kernel.log_transition_prob(origin, destination, shape)
+
+    @property
+    def name(self):
+        return 'MHMoveKernel'
+
+    def walk(self, f):
+        return f(MHMoveKernel(self.target, self.kernel, self._moves))
+
+    def cond(self, qs):
+        return MHMoveKernel(self.target, self.kernel, self._moves,
+                            q=qs[self.name])
+
+    def forward(self, zs, xi, log_weight, *args, **kwargs):
+        multiple_zs = isinstance(zs, tuple)
+        if not multiple_zs:
+            zs = (zs,)
+
+        for t in range(self._moves):
+            xiq, log_weight_q = self.kernel(zs, xi, log_weight, *args,
+                                            **kwargs)
+            zsp, xip, log_w = self.target.cond(xiq)(*args, **kwargs)
+            if not multiple_zs:
+                zsp = (zsp,)
+            log_omega_q = importance.conditioning_factor(xi, xiq,
+                                                         self.batch_shape)
+            log_w = log_weight_q - log_omega_q + log_w
+            log_alpha = utils.batch_mean(
+                torch.min(torch.zeros(self.batch_shape), log_w - log_weight)
+            )
+            if torch.bernoulli(log_alpha.exp()) == 1:
+                zs = zsp
+                xi = xip
+                log_weight = log_w
+
+        return xi, log_weight
