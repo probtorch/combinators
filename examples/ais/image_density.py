0f3af79a6321408aaa00faf30c9c3b1c3ff931db
Heiko Zimmermann <mail@heikozimmermann.com>
2019-03-07 18:19:48 -0500

    vectorized implementation

--- examples/ais/image_density.py
+++ examples/ais/image_density.py
@@ -1,20 +1,32 @@
 import torch
 import numpy
 
-def img_density(img_ary, coord):
-    grid_density = -(img_ary - 255).T
-    x, y = coord.numpy()
+dtype = torch.FloatTensor
+dtype_long = torch.LongTensor
 
-    # Determine grid cell boundaries 
-    xf, yf = numpy.floor(coord).astype(int)
-    xc, yc = numpy.ceil(coord).astype(int)
+def bilinearInterpolation(im, x, y):
+    x0 = torch.floor(x).type(dtype_long)
+    x1 = x0 + 1
     
-    # Bi-linear interpolation for density values at coordinate 
-    density_xy_floor = (xc - x) * grid_density[xf, yf] + (x - xf) * grid_density[xc, yf]
-    density_xy_ceil = (xc - x) * grid_density[xf, yc] + (x - xf) * grid_density[xc, yc]
-    density_xy = (yc - y) * density_xy_floor + (y - yf) * density_xy_ceil
+    y0 = torch.floor(y).type(dtype_long)
+    y1 = y0 + 1
 
-    return density_xy
+    x0 = torch.clamp(x0, 0, im.shape[1]-1)
+    x1 = torch.clamp(x1, 0, im.shape[1]-1)
+    y0 = torch.clamp(y0, 0, im.shape[0]-1)
+    y1 = torch.clamp(y1, 0, im.shape[0]-1)
+    
+    Ia = im[y0, x0]
+    Ib = im[y1, x0]
+    Ic = im[y0, x1]
+    Id = im[y1, x1]
+    
+    wa = (x1.type(dtype) - x) * (y1.type(dtype) - y)
+    wb = (x1.type(dtype) - x) * (y - y0.type(dtype))
+    wc = (x - x0.type(dtype)) * (y1.type(dtype) - y)
+    wd = (x - x0.type(dtype)) * (y - y0.type(dtype))
+
+    return Ia*wa + Ib*wb + Ic*wc + Id*wd
 
 if __name__ == '__main__':
     from scipy.misc import imread 
@@ -22,9 +34,17 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     
     img_ary = imread('probtorch-logo-bw.png', mode='L')
-    img_ary = gaussian_filter(img_ary, sigma=1)
+    img_ary = gaussian_filter(img_ary, sigma=0.1)
+    grid_density = torch.FloatTensor(-(img_ary - 255).T)
+
+    n = 100
+    x = torch.rand(n) * grid_density.shape[0]
+    y = torch.rand(n) * grid_density.shape[1]
+    fxy = bilinearInterpolation(grid_density, x, y).numpy()
     # img_ary = numpy.array([[1.,2.],[3.,4.]]) # Dummy image for testing
 
     plt.matshow(img_ary)
+    plt.scatter(x, y, c='r')
+    plt.scatter(x[(fxy > 0.5).nonzero()], y[(fxy > 0.5).nonzero()], c='b')
     plt.show()
 
