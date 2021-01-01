efd1eb130676733ef0bef126ae2ebe6a1ed52d5e
Sam Stites <stites@users.noreply.github.com>
2020-12-31 23:25:36 -0500

    bump nix dependencies

--- default.nix
+++ default.nix
@@ -1,11 +1,28 @@
 let
+  pynixifyOverlay = self: super: {
+    python3 = super.python3.override { inherit packageOverrides; };
+    python37 = super.python37.override { inherit packageOverrides; };
+    python38 = super.python38.override { inherit packageOverrides; };
+  };
+
+  packageOverrides = self: super: {
+    antlr4-python3-runtime =
+      self.callPackage ./nix/antlr4.nix { };
+
+    hydra-core = self.callPackage ./nix/hydra.nix { };
+
+    # omegaconf = self.callPackage ./packages/omegaconf { };
+
+  };
+
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {
     overlays = [
       (_: _: { inherit sources; })
-      (_: super: {
+      pynixifyOverlay
+      (self: super: {
         mach-nix = import sources.mach-nix {
-          pkgs = super;
+          pkgs = self;
           python = "python38";
           # optionally update pypi data revision from https://github.com/DavHau/pypi-deps-db
           pypiDataRev = "4a14f99";  # 12-22-2020
@@ -17,22 +34,27 @@ let
 in
 
 pkgs.mach-nix.mkPython {
-  requirements = (builtins.readFile ./requirements.txt)  + "\njupyterlab";
+  requirements = (builtins.readFile ./requirements.txt)
+   # + "\njupyterlab\njson5==0.9.5"
+   # + "\nhydra-core==1.0.4"
+  ;
 
   packagesExtra = [
     ./probtorch # local branch starting from nvi-dev on probtorch
     # "https://github.com/probtorch/probtorch/tarball/1a9af26"
   ];
-
   providers = {
     # disallow wheels by default
     _default = "nixpkgs,sdist";
     # allow wheels only for torch
     torch = "wheel";
-    hydra-core = "wheel";
-    jupyterlab = "wheel";
+    # jupyterlab = "wheel";
     torchvision = "wheel";
     Sphinx = "wheel";
     json5 = "wheel";
   };
+  # _.hydra-core.buildInputs.add = [ pkgs.jre ];
+  # _.hydra-core.patches.add = [
+  #   ''substituteInPlace build_helpers/build_helpers.py --replace "java" "${pkgs.jre}/bin/java"''
+  # ];
 }
--- shell.nix
+++ shell.nix
@@ -1,19 +1,36 @@
-{ use_jupyter ? true, start_jupyter ? false }:
+{ use_jupyter ? false }:
 let
   mypython = import ./.;
   sources = import ./nix/sources.nix;
   host_pkgs = (import sources.nixpkgs {});
+  jupyter = import sources.jupyterWith {};
+
+  iPython = jupyter.kernels.iPythonWith {
+    name = "mach-nix-jupyter";
+    python3 = mypython.python;
+    packages = p: (mypython.python.pkgs.selectPkgs p) ++ [
+      p.mypy
+      p.pytest
+      host_pkgs.python-language-server
+    ];
+  };
+
+  jupyterEnvironment = jupyter.jupyterlabWith {
+    kernels = [ iPython ];
+    ## The generated directory for extensions
+    directory = ./.jupyterlab;
+    extraPackages = p: [p.python38Packages.pytest p.mypy p.python-language-server];
+    extraJupyterPath = pkgs:
+      "${mypython}/lib/python3.8/site-packages";
+  };
 in
-# ) + (pkgs.lib.strings.concatStringsSep "\n" [
-#    (lib.optionalString use_jupyter "jupyterlab")
-#  ]);
-host_pkgs.mkShell {
+if use_jupyter
+then jupyterEnvironment.env
+else with host_pkgs;
+mkShell {
   buildInputs = [
     mypython
-    # host_pkgs.mypy
-    # host_pkgs.python-language-server
+    python-language-server
+    black
   ];
-  shellHook = host_pkgs.lib.optionalString (use_jupyter && start_jupyter) ''
-    jupyter lab --notebook-dir=$PWD
-  '';
 }
