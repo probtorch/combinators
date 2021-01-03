d767c3272083cd82d5bcc69599b70d72e05af682
Sam Stites <stites@users.noreply.github.com>
2021-01-03 12:03:54 -0500

    sanitycheck hot mess has paid off

--- tests/e2e/test_1dgaussian.py
+++ tests/e2e/test_1dgaussian.py
@@ -1,12 +1,18 @@
 import torch
 from torch import Tensor
+from torch.distributions import Normal
 import torch.nn as nn
 from combinators import Program, Kernel, Forward, Reverse, Propose, Trace
 from combinators.stochastic import RandomVariable, Provenance
+from probtorch.util import expand_inputs
 from typeguard import typechecked
 from tqdm import trange
 from pytest import mark
+import sparklines
 import combinators.trace.utils as trace_utils
+from combinators.tensor.utils import thash, show
+from typing import Optional
+
 
 class MultivariateNormalKernel(nn.Module):
     def __init__(self, name, dim, dim_in=None, loc=None, cov=None, cov_embedding=None, map=None, reparameterized=True):
@@ -86,17 +92,16 @@ class MultivariateNormalKernel(nn.Module):
 class Net(nn.Module):
     def __init__(self, dim_in, dim_hidden, dim_out):
         super().__init__()
-        self.joint = nn.Sequential(nn.Linear(dim_in, dim_hidden), nn.ReLU())
-        self.mu = nn.Sequential(nn.Linear(dim_hidden, dim_hidden), nn.ReLU(), nn.Linear(dim_hidden, dim_out))
-        # self.sigma = nn.Sequential(nn.Linear(dim_hidden, dim_hidden), nn.ReLU(), nn.Linear(dim_hidden, dim_out))
+        # self.joint = nn.Sequential(nn.Linear(dim_in, dim_hidden), nn.ReLU())
+        self.mu = nn.Sequential(nn.Linear(dim_in, dim_hidden), nn.ReLU(), nn.Linear(dim_hidden, dim_hidden), nn.ReLU(), nn.Linear(dim_hidden, dim_out))
+        # self.sigma = nn.Sequential(nn.Linear(dim_hidden, dim_hidden), nn.ReLU(), nn.Linear(dim_hidden, dim_out), nn.ReLU())
+        self.dim_out = dim_out
 
     def forward(self, x):
-        y = self.joint(x)
-        mu = self.mu(y) + x
-        # Save stdev for later
+        # y = self.joint(x)
+        mu = self.mu(x)
         # sigma = self.sigma(y)
-        # return torch.cat((mu, sigma), dim=-1)
-        return mu
+        return mu, torch.ones([self.dim_out]) # *0.5 # sigma
 
 class Gaussian1d(Program):
     def __init__(self, loc:int, name:str):
@@ -109,19 +114,21 @@ class Gaussian1d(Program):
         return trace.normal(loc=torch.ones(self.size) *self.loc, scale=torch.ones(self.size), name=self.name)
 
 class _Gaussian1d(nn.Module):
-    def __init__(self, loc:int, std:int, name:str):
+    def __init__(self, loc:int, std:int, name:str, num_samples:int):
         super().__init__()
         self.loc = loc
         self.std = std
         self.name = name
         self.size = torch.Size([1])
+        self.expand_samples = lambda ten: ten.expand(num_samples, *ten.size())
 
-    def forward(self, trace_name, cond_trace=None):
+    def forward(self, trace_name, cond_trace=None, num_samples=1):
         trace = Trace()
         out = trace.normal(
-            loc=torch.ones(self.size)*self.loc,
-            scale=torch.ones(self.size)*self.std,
+            loc=self.expand_samples(torch.ones(self.size, requires_grad=True)*self.loc),
+            scale=self.expand_samples(torch.ones(self.size)*self.std),
             value=None if cond_trace is None else cond_trace[trace_name].value,
+
             name=trace_name)
         return out, trace
 
@@ -136,19 +143,45 @@ class _SimpleKernel(nn.Module):
         self.net = Net(dim_in=1, dim_hidden=num_hidden, dim_out=1)
         self.name = name if ix is None else name + "_" + str(ix)
 
-    def forward(self, base_trace, base_name, trace_name, cond_trace=None):
+    def forward(self, base_trace, base_name, trace_name, cond_trace=None, detach_base=False):
+        trace = Trace()
+
+        # INPUT TRACE
+        base_rv = base_trace[base_name]
+        new_val = base_rv.value.detach() if detach_base else base_rv.value
+        # new_val.requires_grad = not detach_base
+        copy_rv = RandomVariable(base_rv.dist, new_val, provenance=Provenance.OBSERVED) # ImproperRV in the general case (improper if improper, random if )
+        trace.append(copy_rv, name=base_name)
+
+        # Kernel code starts here
+        # EXTENDED ARGUMENTS HERE
+        mu, sig = self.net(copy_rv.value.detach())
+        value = None if cond_trace is None else cond_trace[trace_name].value.detach()
+        # import ipdb; ipdb.set_trace();
+
+        out = trace.normal(loc=mu, scale=sig, name=trace_name, value=value)
+
+
+        # ext_rv = RandomVariable(ext_dist, (ext_dist.sample() if cond_trace is None else cond_trace[trace_name].value), provenance=Provenance.SAMPLED)
+        # trace.append(ext_rv, name=trace_name)
+
+        return out, trace
+    # @expand_inputs
+    def xforward(self, base_trace, base_name, trace_name, cond_trace=None):
         trace = Trace()
         # mu, sigma = out[0], out[1]
         base_rv = base_trace[base_name]
-        copy_rv = RandomVariable(base_rv.dist, base_rv.value, provenance=Provenance.OBSERVED)
+        copy_rv = RandomVariable(base_rv.dist, base_rv.value.detach(), provenance=Provenance.OBSERVED)
         trace.append(copy_rv, name=base_name)
-        out = trace.normal(
-            loc=self.net(base_rv.value.detach()),
-            value=None if cond_trace is None else cond_trace[trace_name].value,
-            scale=torch.ones([1]),
-            name=trace_name)
 
-        return out, trace
+        mu, sig = self.net(base_rv.value.detach())
+        # import ipdb; ipdb.set_trace();
+
+        ext_dist = Normal(loc=mu, scale=sig)
+        ext_rv = RandomVariable(ext_dist, (ext_dist.sample() if cond_trace is None else cond_trace[trace_name].value), provenance=Provenance.SAMPLED)
+        trace.append(ext_rv, name=trace_name)
+
+        return ext_rv.value, trace
     def __repr__(self):
         return "[{}]{}".format(self.name, super().__repr__())
 
@@ -314,92 +347,144 @@ def step(k, p_prev, proposal, target, fwd_kernel, rev_kernel, num_steps):
     lv_prop = lv
     return q_ext, p_ext, lv, p_prop, lv_prop
 
+
 def test_Gaussian_sanitycheck():
-    """ The VAE test. At one step no need for any detaches. """
+    torch.manual_seed(1)
     target_mean, target_stdv = 4, 1
-    proposal = _Gaussian1d(loc=target_stdv, std=6, name='g_0')
-    target = _Gaussian1d(loc=target_mean, std=1, name='g_1')
+    num_samples=200
+    lr = 1e-2
+    proposal = _Gaussian1d(loc=target_stdv, std=6, name='g_0', num_samples=num_samples)
+    target = _Gaussian1d(loc=target_mean, std=1, name='g_1', num_samples=num_samples)
     fwd = _SimpleKernel(num_hidden=4, name='f_01')
     rev = _SimpleKernel(num_hidden=4, name='r_10')
-    optimizer = torch.optim.Adam([dict(params=x.parameters()) for x in [proposal, target, fwd, rev]])
+    optimizer = torch.optim.Adam([dict(params=x.parameters()) for x in [proposal, target, fwd, rev]], lr=lr)
 
-    num_steps = 1000
+    num_steps = 150
     loss_ct, loss_sum = 0, 0.0
     loss_all = []
+    loss_every = []
+    def print_traces(ext_only=True):
+        if not ext_only:
+            try:
+                print("p_prv: {}".format(trace_utils.show(p_prv, fix_width=True)))
+            except:
+                print("p_prv: None")
+            try:
+                print("q_prp: {}".format(trace_utils.show(q_prp, fix_width=True)))
+            except:
+                print("q_prp: None")
+        try:
+            print("q_ext: {}".format(trace_utils.show(q_ext, fix_width=True)))
+        except:
+            print("q_ext: None")
+        if not ext_only:
+            try:
+                print("p_tar: {}".format(trace_utils.show(p_tar, fix_width=True)))
+            except:
+                print("p_tar: None")
+        try:
+            print("p_ext: {}".format(trace_utils.show(p_ext, fix_width=True)))
+        except:
+            print("p_ext: None")
+
     with trange(num_steps) as bar:
         for i in bar:
-            _, q_prp = proposal(  trace_name='z_0')
-            _, q_ext = fwd(q_prp, trace_name='z_1', base_name='z_0')
-            _, p_tar = target(    trace_name='z_1',                   cond_trace=q_ext)
-            _, p_ext = rev(p_tar, trace_name='z_0', base_name='z_1',  cond_trace=q_ext)
+            # Initial
+            o, p_prv = proposal(  trace_name='z_0')
+            # sampling reparameterized will have a gradient
+            # NOTE: probtorch will default to running rsample
+            lw = torch.zeros([1])
+            # ================== #
+            # Nestable           #
+            # ================== #
+            # TODO: reduce number of samples, and just fix the samples to something simple
+
+            _, q_prp = proposal(  trace_name='z_0',                 cond_trace=p_prv) # q_prp: {z_0: normal(1,6).detach()}
+            # NOTE: ALL OF p_prv is detached
+
+            # TODO: Remove joint nn. Fix weights of NN to [xavier?]
+            _, q_ext = fwd(q_prp, trace_name='z_1', base_name='z_0')                    # q_ext: {z_0: normal(1,6).detach(), z_1: normal(nnet(q_prp['z_0'].value.detach()),1).detach()}
+
+            # NOTE: z_0 must _not_ have grad
+            # NOTE: z_1 must have grad
+            # NOTE: AVO ONLY WORKS FOR REPARAMETERIZED <<< maybe probtorch does this already. doublecheck
+            #  - check if samples have grad
+
+            # TODO: same as with proposal above
+            _, p_tar = target(    trace_name='z_1',                   cond_trace=q_ext) # p_tar: {z_1: normal(4,1).detach()}
+            # NOTE: z_1 must have grad
+
+            # TODO: same as with fwd above
+            _, p_ext = rev(p_tar, trace_name='z_0', base_name='z_1',  cond_trace=q_ext) # q_ext: {z_0: normal(nnet(p_tar['z_1'].value.detach()), 6).detach(), z_1: normal(4,1).detach()}
+            # NOTE: z_1 must have grad <<< note!!! important for connecting the gradients
+            # NOTE: z_0 must have grad
+            # q_ext['z_0'].value.requires_grad = False
+            # print_traces(ext_only=False)
+            #import ipdb; ipdb.set_trace();
+
+            q_ext = trace_utils.copytrace(q_ext, detach=p_prv.keys())
+            # ================== #
+            # Compute Weight     #
+            # ================== #
 
             assert trace_utils.valeq(q_ext, p_ext, nodes=p_ext._nodes, check_exist=True)
             lv = p_ext.log_joint(batch_dim=None, sample_dims=0, nodes=p_ext._nodes) - \
                 q_ext.log_joint(batch_dim=None, sample_dims=0, nodes=p_ext._nodes)
+            # ^^^^ everything will work out with reparams
+            lw = lw.detach() # completely detached so this is okay with reparam
+            values = -lv
+            # convice yourself: current intro of JMLR + check with forward grad
+            log_weights = torch.zeros_like(lv)
+            sample_dims=0
+            reducedims=(0,)
+            keepdims=False
+            # loss = _estimate_mc(-lv, torch.zeros_like(lv), sample_dims=0, reducedims=(0,), keepdims=False,)
+            nw = torch.nn.functional.softmax(torch.zeros_like(lv), dim=sample_dims)
 
-            loss = _nvo_avo(torch.ones_like(lv.detach(), requires_grad=False), lv, q_ext, p_ext, batch_dim=None)
+            loss = (nw * values).sum(dim=reducedims, keepdim=keepdims)
+
+            # loss = loss_k
             loss = loss.mean()
-
-            # # Initial step
-            # # q_ext = p_ext = trace(proposal, "z_0", sample_shape=sample_shape)
-            # q_ext = p_ext = proposal()
-            # lv = torch.zeros(sample_shape, device=device)
-            # p_prop, lv_prop = p_ext, lv
-            #
-            # # Tracking
-            # loss_0 = _nvo_avo(lw.detach(), lv, q_ext, p_ext)
-            #
-            # # Update loss and cumulative weight -> use proper weight here, e.g. weight after resampling.
-            # loss += loss_0.mean()
-            # lw += lv_prop.detach()
-            #
-            # q_prp = extend_with(  proposal, "z_0", Trace(), cond_trace=p_prev, detach_value=True)
-            # q_ext = extend_with(fwd_kernel, "z_1",   q_prp, cond_trace=None, cond_map={'z_in': 'z_0'})
-            #
-            # # Construct extended target
-            # p_tar = tend_with(    target, "z_1", Trace(), cond_trace=q_ext)
-            # p_ext = tend_with(rev_kernel, "z_0",   p_tar, cond_trace=q_ext, cond_map={'z_in': 'z_1'})
-            #
-            # # IW relabeling:
-            # lv = trace_utils.log_importance_weight(proposal_trace=q_ext, target_trace=p_ext, batch_dim=1, sample_dims=0, check_exist=True)
-            # p_prop = p_ext
-            # lv_prop = lv
-            #
-            # loss_k = _nvo_avo(lw.detach(), lv, q_ext, p_ext)
-            # loss += loss_k.mean()
-            # # lw += lv_prop.detach() # just for logging
 
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             loss_ct += 1
-            loss_scalar = lv.detach().cpu().sum().item()
+            loss_scalar = lv.detach().cpu().mean().item()
             loss_sum += loss_scalar
+            loss_every.append(loss_scalar)
+            if num_steps <= 100:
+               loss_all.append(loss_scalar)
             if i % 10 == 0:
                loss_avg = loss_sum / loss_ct
                loss_template = 'loss={}{:.4f}'.format('' if loss_avg < 0 else ' ', loss_avg)
                bar.set_postfix_str(loss_template)
                loss_ct, loss_sum  = 0, 0.0
-               loss_all.append(loss_avg)
-
-    import sparklines
-    print("min: {:.4f}\nmax: {:.4f}".format(max(loss_all), min(loss_all)))
-    print(sparklines.sparklines(list(map(lambda l: 0 if l < 0 else l, loss_all)))[0])
-    print(sparklines.sparklines(loss_all)[0])
+               if num_steps > 100:
+                   loss_all.append(loss_avg)
 
     with torch.no_grad():
-        num_validate_samples = 100
+        tlosses = torch.tensor(loss_all)
+        lowest_loss = min(loss_all)
+        highest_loss = max(loss_all)
+        print("greatest loss: {:.4f}\nsmallest loss: {:.4f}".format(lowest_loss, highest_loss))
+        print(sparklines.sparklines(((-tlosses) + abs(tlosses.min())).numpy())[0])
+
+        num_validate_samples = 400
         samples = []
         for _ in range(num_validate_samples):
             _, q_prp = proposal(trace_name='z_0')
             out, q_ext = fwd(q_prp, trace_name='z_1', base_name='z_0')
             # out, _ = proposal()
-            samples.append(out.cpu().item())
-        evaluation = torch.tensor(samples)
+            samples.append(out)
+        evaluation = torch.cat(samples)
         eval_mean, eval_stdv = evaluation.mean().item(), evaluation.std().item()
+        print("mean: {:.4f}, std: {:.4f}".format(eval_mean, eval_stdv))
         mean_tol, stdv_tol = 0.25, 0.5
         assert (target_mean - mean_tol) < eval_mean and  eval_mean < (target_mean + mean_tol)
         assert (target_stdv - stdv_tol) < eval_stdv and  eval_stdv < (target_stdv + stdv_tol)
+
+
 
 
 
