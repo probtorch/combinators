553d4edd4d9b25fd9ba8ea7506b835664289030e
Heiko Zimmermann <mail@heikozimmermann.com>
2021-02-12 11:53:58 -0500

    updated test to work with inputs

--- tests/test_inference_v2.py
+++ tests/test_inference_v2.py
@@ -1,12 +1,6 @@
 import torch
-from combinators.inference import *
-from combinators.program import *
-from combinators.densities import *
-from combinators.densities.kernels import *
-
+from combinators.inference import Program, Compose, Extend, Propose, Resample, Condition
 import combinators.debug as debug
-from combinators.stochastic import *
-from combinators.utils import *
 
 class Simple1(Program):
     def __init__(self):
@@ -18,6 +12,7 @@ class Simple1(Program):
 
         trace.normal(loc=torch.ones(1), scale=torch.ones(1), value=z_1, name="x_1")
         trace.normal(loc=torch.ones(1), scale=torch.ones(1), value=z_2, name="x_2")
+        return torch.tensor(3.)
 
 class Simple2(Program):
     def __init__(self):
@@ -29,50 +24,47 @@ class Simple2(Program):
 
         trace.normal(loc=torch.ones(1), scale=torch.ones(1), value=z_2, name="x_2")
         trace.normal(loc=torch.ones(1), scale=torch.ones(1), value=z_3, name="x_3")
+        return torch.tensor(1.)
 
-# ============================================================================= #
-# FIXME: We should write some tests that use c since the following are kernels  #
-# ============================================================================= #
 class Simple3(Program):
     def __init__(self):
         super().__init__()
 
-    def model(self, trace, c=None):
-        z_3 = trace.normal(loc=torch.ones(1)*3, scale=torch.ones(1)*3, name="z_3")
+    def model(self, trace, c):
+        z_3 = trace.normal(loc=torch.ones(1)*c, scale=torch.ones(1)*3, name="z_3")
         trace.normal(loc=torch.ones(1), scale=torch.ones(1), value=z_3, name="x_3")
 
 class Simple4(Program):
     def __init__(self):
         super().__init__()
 
-    def model(self, trace, c=None):
-        z_1 = trace.normal(loc=torch.ones(1), scale=torch.ones(1), name="z_1")
+    def model(self, trace, c):
+        z_1 = trace.normal(loc=torch.ones(1)*c, scale=torch.ones(1), name="z_1")
+
+# ============================================================================= #
+# FIXME: We should write some tests that use c since the following are kernels  #
+# ============================================================================= #
 
 def test_run_a_primitive_program():
-    s1 = Simple1()
-    s1_out = s1()
+    s1_out = Simple1()(None)
+    assert set(s1_out.trace.keys()) == {"z_1", "z_2", "x_1", "x_2"}
+    assert s1_out.log_weight == s1_out.trace.log_joint(nodes={"x_1", "x_2"})
 
 def test_cond_eval():
-    s1 = Simple1()
-    s1_out = s1()
-
-    s2 = Condition(program=Simple2(), cond_trace=s1_out.trace)
-    s2_out = s2()
+    s1_out = Simple1()(None)
+    s2_out = Condition(program=Simple2(), cond_trace=s1_out.trace)(None)
 
     rho_f_addrs = {'x_2', 'x_3', 'z_2', 'z_3'}
     tau_f_addrs = {'z_2', 'z_3'}
     tau_p_addrs = {'z_1', 'z_2'}
     nodes = rho_f_addrs - (tau_f_addrs - tau_p_addrs)
-    expected = s2_out.trace.log_joint(nodes=nodes)
+    lw_out = s2_out.trace.log_joint(nodes=nodes)
 
-    assert ( expected == s2_out.log_weight ).all()
-
-    assert len(set({'z_1', 'x_1'}).intersection(set(s2_out.trace.keys()))) == 0
+    assert (lw_out == s2_out.log_weight).all(), "lw_out"
+    assert len(set({'z_1', 'x_1'}).intersection(set(s2_out.trace.keys()))) == 0, "tr_out"
 
 def test_propose():
-    prg = Propose(p=Simple2(), q=Simple1())
-    out = prg(_debug=True)
-
+    out = Propose(p=Simple2(), q=Simple1())(None, _debug=True)
 
     rho_q_addrs = {'x_1', 'x_2', 'z_1', 'z_2'}
     tau_q_addrs = {'z_1', 'z_2'}
@@ -82,12 +74,16 @@ def test_propose():
     assert set(out.q_out.trace.keys()) == {"z_1", "z_2", "x_1", "x_2"}
     assert set(out.p_out.trace.keys()) == {"z_2", "z_3", "x_2", "x_3"}
 
+    # Compute stuff the same way as in the propose combinator to ensure numeric reproduceability 
     lu_1 = out.q_out.trace.log_joint(nodes=nodes)
-    lw_out = out.q_out.log_weight + out.p_out.log_weight - lu_1
+    lu_star = 0
+    lw_1 = out.q_out.log_weight
+    lv = out.p_out.log_weight - (lu_1 + lu_star)
+    lw_out = lw_1 + lv
     assert torch.equal(lw_out, out.log_weight), "lw_out"
 
 def test_compose():
-    out = Compose(q1=Simple1(), q2=Simple3())()
+    out = Compose(q1=Simple1(), q2=Simple3())(None)
 
     assert set(out.trace.keys()) == {'x_1', 'x_2', 'x_3', 'z_1', 'z_2', 'z_3'}
     assert torch.equal(out.q1_out.log_weight, out.q1_out.trace.log_joint(nodes={'x_1', 'x_2'}))
@@ -95,17 +91,14 @@ def test_compose():
     assert torch.equal(out.log_weight, out.q1_out.log_weight + out.q2_out.log_weight)
 
 def test_extend_unconditioned():
-    prg = Extend(p=Simple2(), f=Simple4())
-    out = prg(_debug=True)
+    out = Extend(p=Simple2(), f=Simple4())(None, _debug=True)
     assert set(out.trace.keys()) == {'x_2', 'x_3', 'z_2', 'z_3'}
     assert set(out.trace_star.keys()) == {'z_1'}
     assert torch.equal(out.log_weight, out.p_out.log_weight + out.f_out.trace.log_joint())
 
 def test_extend_conditioned():
-    Q = Compose(q1=Simple1(), q2=Simple3())
-    P = Extend(p=Simple2(), f=Simple4())
-    q_out = Q()
-    p_out = Condition(program=P, cond_trace=q_out.trace)()
+    q_out = Compose(q1=Simple1(), q2=Simple3())(None)
+    p_out = Condition(Extend(p=Simple2(), f=Simple4()), cond_trace=q_out.trace)(None)
     tau_0  = {'z_1', 'z_2', 'z_3', 'x_2', 'x_3', 'x_1'}
     assert set(q_out.trace.keys()) == tau_0
 
@@ -134,7 +127,7 @@ def test_extend_propose():
     debug.seed(7)
     Q = Compose(q1=Simple1(), q2=Simple3())
     P = Extend(p=Simple2(), f=Simple4())
-    out = Propose(p=P, q=Q)()
+    out = Propose(p=P, q=Q)(None)
 
     # Test compose inside propose
     tau_1  = {'z_1', 'z_2', 'z_3', 'x_2', 'x_3', 'x_1'}
@@ -156,9 +149,13 @@ def test_extend_propose():
         + out.p_out.trace_star.log_joint(nodes={'z_1'})
     assert lw_2 == out.p_out.log_weight
 
+    # Compute weight the same way as inside the propose combinator for reproduceability
     lu_1   = out.q_out.trace.log_joint(nodes={'x_2', 'x_3', 'x_1', 'z_2', 'z_3'})
-    lw_out = lw_1 + (lw_2 - (lu_1 + out.p_out.trace_star.log_joint(nodes={"z_1"})))
+    lu_star = out.p_out.trace_star.log_joint(nodes={"z_1"})
+    lv = lw_2 - (lu_1 + lu_star)
+    lw_out = lw_1 + lv
     #   (x_1 x_2 x_3) * ((z_2 z_3  *  x_2 x_3) * (z_1))
     #  ---------------------------------------    
     #  ((x_1 x_2 x_3  *   z_2 z_3)             * (z_1))
     assert lw_out == out.log_weight, "final is weight"
+    print(lw_out)
