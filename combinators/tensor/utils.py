dbb18d1a6a82cfa07425e8ed6d543c86203e9d04
Sam Stites <stites@users.noreply.github.com>
2021-05-19 13:39:05 -0400

    run utils through black

--- combinators/tensor/utils.py
+++ combinators/tensor/utils.py
@@ -9,14 +9,14 @@ import os
 
 
 @typechecked
-def autodevice(preference:Union[int, str, None, torch.device]=None)->torch.device:
+def autodevice(preference: Union[int, str, None, torch.device] = None) -> torch.device:
     if isinstance(preference, torch.device):
         return preference
     else:
         if isinstance(preference, str):
             devstr = preference
         else:
-            if not torch.cuda.is_available() or os.getenv('CUDA_VISIBLE_DEVICES') == '':
+            if not torch.cuda.is_available() or os.getenv("CUDA_VISIBLE_DEVICES") == "":
                 devstr = "cpu"
             elif isinstance(preference, int):
                 devstr = f"cuda:{preference}"
@@ -26,27 +26,29 @@ def autodevice(preference:Union[int, str, None, torch.device]=None)->torch.devic
 
 
 @typechecked
-def kw_autodevice(preference:Union[int, str, None]=None)->Dict[str, torch.device]:
+def kw_autodevice(preference: Union[int, str, None] = None) -> Dict[str, torch.device]:
     return dict(device=autodevice(preference))
 
 
 @typechecked
-def _hash(t:Tensor, length:int)->str:
+def _hash(t: Tensor, length: int) -> str:
     hasher = hashlib.sha1(pickle.dumps(t))
-    return base64.urlsafe_b64encode(hasher.digest()[:length]).decode('ascii')
+    return base64.urlsafe_b64encode(hasher.digest()[:length]).decode("ascii")
 
 
 @typechecked
-def thash(aten:Tensor, length:int=8, with_ref=False, no_grad_char:str=" ")->str:
+def thash(
+    aten: Tensor, length: int = 8, with_ref=False, no_grad_char: str = " "
+) -> str:
     g = "∇" if aten.grad_fn is not None else no_grad_char
     save_ref = aten.detach()
     if with_ref:
         r = _hash(save_ref, (length // 4))
-        v = _hash(save_ref.cpu().numpy(), 3*(length // 4))
-        return f'#{g}{r}{v}'
+        v = _hash(save_ref.cpu().numpy(), 3 * (length // 4))
+        return f"#{g}{r}{v}"
     else:
         v = _hash(save_ref.cpu().numpy(), length)
-        return f'#{g}{v}'
+        return f"#{g}{v}"
 
 
 def prettyshape(size):
@@ -54,6 +56,6 @@ def prettyshape(size):
 
 
 @typechecked
-def show(aten:Tensor)->str:
+def show(aten: Tensor) -> str:
     t = str(aten.dtype).split(".")[-1]
     return f"{t}{prettyshape(aten.size())}{thash(aten)}"
--- combinators/trace/utils.py
+++ combinators/trace/utils.py
@@ -7,7 +7,13 @@ from torch import distributions as D
 from typing import Callable, Any, Tuple, Optional, Set, Union, Dict, List
 from typeguard import typechecked
 
-from probtorch.stochastic import Trace, Provenance, _RandomVariable, RandomVariable, ImproperRandomVariable
+from probtorch.stochastic import (
+    Trace,
+    Provenance,
+    _RandomVariable,
+    RandomVariable,
+    ImproperRandomVariable,
+)
 
 import combinators.tensor.utils as tensor_utils
 
@@ -17,29 +23,54 @@ TraceLike = Union[Trace, Dict[str, Union[Tensor, _RandomVariable]]]
 def distprops(dist):
     """ return a list of a distribution's properties """
     return [
-        p for p in (set(inspect.getfullargspec(dist.__init__).args) - {'self'})
-            if hasattr(dist, p)
+        p
+        for p in (set(inspect.getfullargspec(dist.__init__).args) - {"self"})
+        if hasattr(dist, p)
     ]
+
 
 def distprops_to_kwargs(dist):
     return {p: getattr(dist, p) for p in distprops(dist)}
 
-@typechecked
-def maybe_sample(trace:Optional[Trace], sample_shape:Union[Tuple[int], None, tuple], reparameterized:Optional[bool]=None) -> Callable[[D.Distribution, str], Tuple[Tensor, Provenance, bool]]:
 
-    def curried(dist:D.Distribution, name:str) -> Tuple[Tensor, Provenance, bool]:
-        _reparameterized = reparameterized if reparameterized is not None else dist.has_rsample
+@typechecked
+def maybe_sample(
+    trace: Optional[Trace],
+    sample_shape: Union[Tuple[int], None, tuple],
+    reparameterized: Optional[bool] = None,
+) -> Callable[[D.Distribution, str], Tuple[Tensor, Provenance, bool]]:
+    def curried(dist: D.Distribution, name: str) -> Tuple[Tensor, Provenance, bool]:
+        _reparameterized = (
+            reparameterized if reparameterized is not None else dist.has_rsample
+        )
         if trace is not None and name in trace:
             return trace[name].value, Provenance.REUSED, _reparameterized
         elif sample_shape is None:
-            return (dist.rsample() if _reparameterized else dist.sample(), Provenance.SAMPLED, _reparameterized)
+            return (
+                dist.rsample() if _reparameterized else dist.sample(),
+                Provenance.SAMPLED,
+                _reparameterized,
+            )
         else:
-            return (dist.rsample(sample_shape) if _reparameterized else dist.sample(sample_shape), Provenance.SAMPLED, _reparameterized)
+            return (
+                dist.rsample(sample_shape)
+                if _reparameterized
+                else dist.sample(sample_shape),
+                Provenance.SAMPLED,
+                _reparameterized,
+            )
+
     return curried
 
 
 @typechecked
-def valeq(t1:Trace, t2:Trace, nodes:Union[Dict[str, Any], List[Tuple[str, str]], None]=None, check_exist:bool=True, strict:bool=False)->bool:
+def valeq(
+    t1: Trace,
+    t2: Trace,
+    nodes: Union[Dict[str, Any], List[Tuple[str, str]], None] = None,
+    check_exist: bool = True,
+    strict: bool = False,
+) -> bool:
     """
     Two traces are value-equivalent for a set of names iff the values of the corresponding names in both traces exist
     and are mutually equal.
@@ -47,32 +78,48 @@ def valeq(t1:Trace, t2:Trace, nodes:Union[Dict[str, Any], List[Tuple[str, str]],
     check_exist::boolean    Controls if a name must exist in both traces, if set to False one values of variables that
                             exist in both traces are checks for 'value equivalence'
     """
-    t1nodes:Set[str] = set(t1._nodes.keys())
-    t2nodes:Set[str] = set(t2._nodes.keys())
+    t1nodes: Set[str] = set(t1._nodes.keys())
+    t2nodes: Set[str] = set(t2._nodes.keys())
     if nodes is None:
         _nodes = t1nodes.union(t2nodes) if strict else t1nodes.intersection(t2nodes)
-    elif isinstance(nodes, list) and len(nodes) > 0 and isinstance(nodes[0], tuple) and len(nodes[0]) == 2:
-        raise NotImplementedError('pairing tuples not supported yet')
+    elif (
+        isinstance(nodes, list)
+        and len(nodes) > 0
+        and isinstance(nodes[0], tuple)
+        and len(nodes[0]) == 2
+    ):
+        raise NotImplementedError("pairing tuples not supported yet")
     else:
         _nodes = nodes
 
     if not all(name in t1 and name in t2 for name in _nodes):
         str_t1nodes = "{" + ", ".join(t1nodes) + "}"
         str_t2nodes = "{" + ", ".join(t2nodes) + "}"
-        cmpr_string = "are not in {" + ", ".join(_nodes) + "}" if nodes is not None \
+        cmpr_string = (
+            "are not in {" + ", ".join(_nodes) + "}"
+            if nodes is not None
             else "are not equal"
-        raise Exception("trace's keys {}:\n  trace 1: {}\n  trace 2: {}".format(cmpr_string, str_t1nodes, str_t2nodes))
+        )
+        raise Exception(
+            "trace's keys {}:\n  trace 1: {}\n  trace 2: {}".format(
+                cmpr_string, str_t1nodes, str_t2nodes
+            )
+        )
 
     invalid = list(filter(lambda name: not trace_eq(t1, t2, name), _nodes))
     if len(invalid) > 0:
         str_nodes = "{" + ", ".join(invalid) + "}"
-        raise Exception("RV nodes {} are not equal in:\n  trace 1: {}\n  trace 2: {}".format(str_nodes, set(t1.keys()), set(t2.keys())))
+        raise Exception(
+            "RV nodes {} are not equal in:\n  trace 1: {}\n  trace 2: {}".format(
+                str_nodes, set(t1.keys()), set(t2.keys())
+            )
+        )
 
     return True
 
 
 @typechecked
-def show(tr:TraceLike, fix_width=False):
+def show(tr: TraceLike, fix_width=False):
     """ show a trace """
     get_value = lambda v: v if isinstance(v, Tensor) else v.value
     ten_show = lambda v: tensor_utils.show(get_value(v), fix_width=fix_width)
@@ -80,28 +127,30 @@ def show(tr:TraceLike, fix_width=False):
 
 
 def showDist(dist):
-    ''' prettier show instance for distributions more information when debugging '''
+    """ prettier show instance for distributions more information when debugging """
     props = distprops(dist)
-    sattrs = [f'{p}:{tensor_utils.show(getattr(dist, p))}' for p in props]
-    return type(dist).__name__ + "(" +", ".join(sattrs)+ ")"
+    sattrs = [f"{p}:{tensor_utils.show(getattr(dist, p))}" for p in props]
+    return type(dist).__name__ + "(" + ", ".join(sattrs) + ")"
 
 
 def showRV(v, args=[], dists=False):
-    ''' prettier show instance for RandomVariables with more information when debugging '''
+    """ prettier show instance for RandomVariables with more information when debugging """
     if len(args) == 0 and not dists:
         print("[WARNING] asked to show a RV, but no arguments passed")
     name = ""
-    arglist = [f'{a}={tensor_utils.show(getattr(v, a))}' for a in args]
+    arglist = [f"{a}={tensor_utils.show(getattr(v, a))}" for a in args]
     if dists:
         name = type(v.dist).__name__ + "("
         props = distprops(v.dist)
-        sattrs = [f'{p}={tensor_utils.show(getattr(v.dist, p))}' for p in props]
-        arglist.append('dist=' + name + "(" + ", ".join(sattrs) + ")")
+        sattrs = [f"{p}={tensor_utils.show(getattr(v.dist, p))}" for p in props]
+        arglist.append("dist=" + name + "(" + ", ".join(sattrs) + ")")
     return name + ", ".join(arglist)
 
 
 @typechecked
-def showall(tr:Trace, delim="; ", pretty=True, mlen=0, sort=True, args=[], dists=False):
+def showall(
+    tr: Trace, delim="; ", pretty=True, mlen=0, sort=True, args=[], dists=False
+):
     """ show instance for a trace, using showRV """
     items = list(tr.items())
     if len(items) == 0:
@@ -113,28 +162,39 @@ def showall(tr:Trace, delim="; ", pretty=True, mlen=0, sort=True, args=[], dists
         items.sort()
     pref = delim if "\n" == delim[0] or "\n" == delim[-1] else ""
 
-    return "{" + pref + delim.join([("{:>"+str(mlen)+"}-➢{}").format(k, showRV(v, args=args, dists=dists)) for k, v in items]) + "}"
+    return (
+        "{"
+        + pref
+        + delim.join(
+            [
+                ("{:>" + str(mlen) + "}-➢{}").format(
+                    k, showRV(v, args=args, dists=dists)
+                )
+                for k, v in items
+            ]
+        )
+        + "}"
+    )
 
 
 @typechecked
-def showvals(tr:Trace, delim="; ", pretty=True, mlen=0, sort=True):
-    return showall(tr, args=['value'])
+def showvals(tr: Trace, delim="; ", pretty=True, mlen=0, sort=True):
+    return showall(tr, args=["value"])
 
 
 @typechecked
-def showprobs(tr:Trace, delim="; ", pretty=True, mlen=0, sort=True):
-    return showall(tr, args=['log_prob'])
+def showprobs(tr: Trace, delim="; ", pretty=True, mlen=0, sort=True):
+    return showall(tr, args=["log_prob"])
 
 
 @typechecked
-def showdists(tr:Trace, delim="; ", pretty=True, mlen=0, sort=True):
+def showdists(tr: Trace, delim="; ", pretty=True, mlen=0, sort=True):
     return showall(tr, args=[], dists=True)
 
 
 @typechecked
-def trace_eq(t0:Trace, t1:Trace, name:str):
+def trace_eq(t0: Trace, t1: Trace, name: str):
     return name in t0 and name in t1 and torch.equal(t0[name].value, t1[name].value)
-
 
 
 @unique
